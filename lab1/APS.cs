88c6ba52f3cdd08c7343c70d21ae45bd29f4522f
YarPodl <yar.podlesnov@yandex.ru>
2018-02-10 17:29:09 +0300

    Начало

---
+++ lab1/APS.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class APS
+    {
+        public String name;
+        public int number;
+        public String addres;
+        public int countUsers;
+        public float usersPay;
+        public String tarif;
+        public int freeLines;
+
+
+
+        public APS()
+        {
+            name = "";
+            number = 0;
+            addres = "";
+            countUsers = 0;
+            usersPay = 0;
+            tarif = "";
+            freeLines = 0;
+        }
+        public APS(String name)
+        {
+            this.name = name;
+            number = 0;
+            addres = "";
+            countUsers = 0;
+            usersPay = 0;
+            tarif = "";
+            freeLines = 0;
+        }
+        public APS(String name, int number)
+        {
+            this.name = name;
+            this.number = number;
+            addres = "";
+            countUsers = 0;
+            usersPay = 0;
+            tarif = "";
+            freeLines = 0;
+        }
+
+        public APS(string name, int number, string addres, int countUsers, float usersPay, string tarif, int freeLines)
+        {
+            this.name = name;
+            this.number = number;
+            this.addres = addres;
+            this.countUsers = countUsers;
+            this.usersPay = usersPay;
+            this.tarif = tarif;
+            this.freeLines = freeLines;
+        }
+
+        public override String ToString() 
+        {
+            return "Название: "
+                + name
+                + "\nномер: "
+                + number.ToString()
+                + "\nадрес: "
+                + addres
+                + "\nколичество пользователей: "
+                + countUsers.ToString()
+                + "\nАбонентская плата: "
+                + usersPay.ToString()
+                + "\nтариф: "
+                + tarif
+                + "\nсвободные линии: "
+                + freeLines.ToString();
+        }
+        public void printName()
+        {
+            Console.WriteLine(name);
+        }
+        public void printNumber16()
+        {
+            Console.WriteLine(String.Format());
+        }
+    }
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            APS t = new APS("Наша станция", 771);
+            Console.WriteLine(t.ToString());
+            Console.WriteLine();
+            t.printNumber16();
+        }
+    }
+}
